910114e3024c8fa31fb35992fa674f9cb30d17c7
Tomáš Korec <tom.korec@gmail.com>
2017-09-30 14:46:49 +0200

    count immediately

--- www/js/main.js
+++ www/js/main.js
@@ -5,3 +5,22 @@ $('#mainForm').click(function(){
         $('#sendForm').attr("disabled","disabled");   
     }
 });
+var totalPrice = 0;
+$('input[type=checkbox]').change(function(){
+    
+    if($(this).is(":checked")){
+        $(this).each(function(){
+            totalPrice = totalPrice + parseInt($(this).siblings('.price').attr('value'));
+        });
+        $('.totalPrice').text(totalPrice);
+        $(this).parent().addClass("checkedBg");
+
+        
+    }else{
+        $(this).each(function(){
+            totalPrice = totalPrice - parseInt($(this).siblings('.price').attr('value'));
+        });
+        $('.totalPrice').text(totalPrice);
+        $(this).parent().removeClass("checkedBg");
+    }
+});
