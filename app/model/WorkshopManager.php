8f969c5c73c95f7715d7a53078c90374ca0f2f4a
Tomáš Korec <tom.korec@gmail.com>
2017-09-30 13:09:25 +0200

    added function checkAllOverlaps & checkNewOverlap & checkTimeOverlap

--- app/model/WorkshopManager.php
+++ app/model/WorkshopManager.php
@@ -21,10 +21,39 @@ class WorkshopManager
 	{
 		$this->database = $database;
 	}
+        
+        public function checkAllOverlaps($validWorkshops){
+            $overlapState = array();
+            foreach ($validWorkshops as $currentWorkshop){
+                foreach ($validWorkshops as $compareWorkshop){
+                    if($currentWorkshop->id != $compareWorkshop->id)
+                        $checkOverlapState = $this->checkTimeOverlap($currentWorkshop->time_from, $currentWorkshop->time_to, $compareWorkshop->time_from, $compareWorkshop->time_to);
+                        array_push ($overlapState, $checkOverlapState);
+                }
+            }
+            return $overlapState;
+        }
+        
+        public function checkNewOverlap($newWorkshop,$currentWorkshop){
+                       
+            $checkOverlapState = $this->checkTimeOverlap($currentWorkshop->time_from, $currentWorkshop->time_to, $newWorkshop->time_from, $newWorkshop->time_to);
+            
+            if($checkOverlapState > 0){
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
 
-	public function wshopFormSuccess($form, $values){
-		echo "string";
-		return;
-	}
+                public  function checkTimeOverlap($start_date_last, $end_date_last, $start_date_next, $end_date_next){
+            if($start_date_last <= $end_date_next && $end_date_last >= $end_date_next){
+                return 1;
+            }
+            else {
+                
+                return 0;
+            }
+        }
 	
 }
